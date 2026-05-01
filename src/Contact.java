564d4a9a0f347acaa261fc0fa317bee177d59f31
christiandev <christianakakpo13@gmailcom>
2026-05-01 13:59:21 +0000

    Ajout et recherche d'un contact

---
+++ src/Contact.java
@@ -0,0 +1,5 @@
+class Contact {
+    // Attributs publics du contact
+    public String name;
+    public String phoneNumber;
+}
---
+++ src/ContactManager.java
@@ -0,0 +1,26 @@
+class ContactsManager {
+    Contact[] myFriends;
+    int friendsCount;
+
+    // Constructeur par défaut
+    ContactsManager() {
+        this.friendsCount = 0;
+        this.myFriends = new Contact[500];
+    }
+
+    // Ajouter un contact au tableau
+    void addContact(Contact contact) {
+        myFriends[friendsCount] = contact;
+        friendsCount++;
+    }
+
+    // Rechercher un contact par son nom
+    Contact searchContact(String searchName) {
+        for (int i = 0; i < friendsCount; i++) {
+            if (myFriends[i].name.equals(searchName)) {
+                return myFriends[i];
+            }
+        }
+        return null; // Pas trouvé
+    }
+}

f0e082b0d7f6cb50b969d277240833dcc4b96295
christiandev <christianakakpo13@gmailcom>
2026-05-01 14:01:28 +0000

    Rechercher et afficher un contact

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-//public class Main {
-//    public static void main(String[] args) {
-//        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-//        // to see how IntelliJ IDEA suggests fixing it.
-//        System.out.printf("Hello and welcome!");
-//
-//        for (int i = 1; i <= 5; i++) {
-//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//            System.out.println("i = " + i);
-//        }
-//    }
-//}
+class Main {
+    public static void main(String[] args) {
+        ContactsManager mgr = new ContactsManager();
+
+        // Créer et ajouter 5 contacts
+        Contact c1 = new Contact();
+        c1.name = "Alice";
+        c1.phoneNumber = "0701234567";
+        mgr.addContact(c1);
+
+        Contact c2 = new Contact();
+        c2.name = "Bob";
+        c2.phoneNumber = "0712345678";
+        mgr.addContact(c2);
+
+        Contact c3 = new Contact();
+        c3.name = "Chloé";
+        c3.phoneNumber = "0723456789";
+        mgr.addContact(c3);
+
+        Contact c4 = new Contact();
+        c4.name = "David";
+        c4.phoneNumber = "0734567890";
+        mgr.addContact(c4);
+
+        Contact c5 = new Contact();
+        c5.name = "Emma";
+        c5.phoneNumber = "0745678901";
+        mgr.addContact(c5);
+
+        // Rechercher et afficher un contact
+        Contact found = mgr.searchContact("Chloé");
+        if (found != null) {
+            System.out.println("Trouvé : " + found.phoneNumber);
+        } else {
+            System.out.println("Contact introuvable.");
+        }
+    }
+}
